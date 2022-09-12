835590146081c406a8a161ae590f89f3cf38c9b9
ejackson1228 <ejackson1228@gmail.com>
2022-09-12 16:46:16 -0400

    created player objetc with name, strength, health, and agility

---
+++ __tests__/Player.test.js
@@ -0,0 +1,16 @@
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
+test('creates a player object', () => {
+    const player = new Player('dave');
+
+    expect(player.name).toBe('dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+}) 
